4325822b5243762d8ed4fe5cf62728ea198178ee
Conner Gleason <connereg@optonline.net>
2021-08-11 09:09:29 -0400

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+function superbowlWin(record){
+    //Made a Function called 'superbowlWin' that takes in the array named 'record' to use in the following function
+    const winningYears = record.find( record => record.result === 'W');
+    // make a Variable named 'Winning Years' that takes in all values in the Array named 'Record' that are FOUND TO HAVE A 'W' IN THE 'Result' Parameter!
+    console.log(winningYears)
+    return  !!winningYears ? winningYears.year : undefined;
+    //   ^ Translates: NOT inverse of 'Winning Years' uses ? (shorthand for if) to either return a 'Year' from 'Record' when TRUE, if NOT TRUE then return 'Undefined'
+}
