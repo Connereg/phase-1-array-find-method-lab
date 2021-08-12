413c0d2180fd12ed7ee1649375e82a6df8954d26
Conner Gleason <connereg@optonline.net>
2021-08-12 11:16:16 -0400

    commit tree d611704308b80500155d3e33789f62097225ac4d

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 function superbowlWin(record){
     //Made a Function called 'superbowlWin' that takes in the array named 'record' to use in the following function
-    const winningYears = record.find( record => record.result === 'W');
-    // make a Variable named 'Winning Years' that takes in all values in the Array named 'Record' that are FOUND TO HAVE A 'W' IN THE 'Result' Parameter!
-    console.log(winningYears)
-    return  !!winningYears ? winningYears.year : undefined;
+    const winningYear = record.find( eachYear => eachYear.result === 'W');
+    // make a Variable named 'Winning Years' that MOVES THROUGH all objects in the Array and returns the FIRST match in 'Record' that are FOUND TO HAVE A 'W' IN THE 'Result' Parameter!
+    console.log(winningYear)
+   
+    
+    
+    return winningYear ? winningYear.year : undefined;
     //   ^ Translates: NOT inverse of 'Winning Years' uses ? (shorthand for if) to either return a 'Year' from 'Record' when TRUE, if NOT TRUE then return 'Undefined'
 }
